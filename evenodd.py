621803ffd9db5eee64cfae225ea0b4cd3ac107f1
Suraj Prathipati <prattipatisuraj@gmail.com>
2026-01-03 09:16:37 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,16 @@
+# Program to calculate even and odd count based on input value
+
+# Taking the number of test cases
+t = int(input("Enter testcase: "))
+
+while t > 0:
+    t -= 1
+    # Taking input for 'n'
+    n = int(input("Enter n: "))
+
+    # Calculating even and odd counts
+    even = n // 2
+    odd = n // 2 + 1 if n % 2 != 0 else even
+
+    # Printing the results
+    print(f"Even: {even} Odd: {odd}")
